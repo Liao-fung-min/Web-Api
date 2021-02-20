e4c873902946682c66524d825ef25989f79e9edc
LAPTOP-OQ6LPUCC\alex <alex.tw@nikkyocars.com>
2021-02-20 18:21:20 +0800

    Web API建置並且使用JWT

---
+++ WebApi_jwt/Controllers/LogginController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using WebApi_jwt.Model;
+
+namespace WebApi_jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LogginController(IConfiguration config)
+        {
+
+            _config = config;
+
+        }
+        [HttpGet]
+        public IActionResult Login(string username, string password)
+        {
+
+            UserModel login = new UserModel();
+            login.Username = username;
+            login.Password = password;
+            IActionResult responese = Unauthorized();
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+
+                var tokenstr = GenerateJSONWebToken(user);
+                responese = Ok(new { token = tokenstr });
+
+            }
+
+        }
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.Username == "ALEX" && login.Password == "123")
+            {
+                user = new UserModel { Username = "alex", EmailAddress = "AAAA@gmail.com", Password = "123" };
+
+            }
+            return user;
+        }
+
+        private string GenerateJSONWebToken(UserModel userinfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt: Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var clasmis = new[]
+            {
+            new Claim(JwtRegisteredClaimNames.Sub, userinfo.Username),
+            new Claim(JwtRegisteredClaimNames.Email, userinfo.EmailAddress),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+
+            var token = new JwtSecurityToken
+                (
+                    issuer: _config["Jwt:Issur"],
+                    audience: _config["Jwt:Issur"],
+                    clasmis,
+                    expires: DateTime.Now.AddSeconds(60),
+                    signingCredentials: credentials
+                );
+        }
+    }
+
+}
+
+
+
---
+++ WebApi_jwt/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi_jwt
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // STEP1: 設定用哪種方式驗證 HTTP Request 是否合法
+            services
+                // 檢查 HTTP Header 的 Authorization 是否有 JWT Bearer Token
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                // 設定 JWT Bearer Token 的檢查選項
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
